cfe2135a82a1ad2cb600dcce66540f657f916e66
mrenninger <me@michaelrenninger.com>
2014-12-29 10:31:33 -0800

    initial commit

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var convertedLbl: UILabel!
+    @IBOutlet weak var humanYearsTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onConvertBtnClicked(sender: UIButton) {
+        let conversionFactor = 7.0
+        convertedLbl.hidden = false;
+//        convertedLbl.text = "\(Double((humanYearsTF.text as NSString).doubleValue) / conversionFactor) years in human years"
+        
+        convertedLbl.text = "\(Double((humanYearsTF.text as NSString).doubleValue) * conversionFactor) years in human years"
+        humanYearsTF.resignFirstResponder()
+    }
 
+    @IBAction func onConvertRealBtnClicked(sender: UIButton) {
+        convertedLbl.hidden = false;
+        
+        var ageDbl = Double((humanYearsTF.text as NSString).doubleValue)
+        var convertedAge = 0.0
+        
+        if Double((humanYearsTF.text as NSString).doubleValue) <= 2 {
+            convertedAge = ageDbl * 10.5
+        } else {
+            convertedAge = ((ageDbl - 2) * 4) + 21
+        }
+        
+        convertedLbl.text = "\(convertedAge) years in human years"
+        humanYearsTF.resignFirstResponder()
+    }
 }
 
